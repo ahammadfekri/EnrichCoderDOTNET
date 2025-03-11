04effbca5cd38113be7f54da81972595495c1779
Ahammad Fekri <pingkucis@hotmail.com>
2025-03-11 16:59:48 +0600

    admin

--- WCRM/WCRM/WCRM.ADMIN/obj/Debug/net8.0/WCRM.ADMIN.AssemblyInfo.cs
+++ WCRM/WCRM/WCRM.ADMIN/obj/Debug/net8.0/WCRM.ADMIN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WCRM.ADMIN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41797522e6db7ae94556318e0bae2728e9091962")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb404b284ddbf31b264f6c8908c81bef83fb0212")]
 [assembly: System.Reflection.AssemblyProductAttribute("WCRM.ADMIN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WCRM.ADMIN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
